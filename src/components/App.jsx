c3791024a448419367f478030f65b81ffcf1211a
Cesar Bahena <cabahenac@gmail.com>
2023-02-11 18:12:10 -0600

    Added functionallity (not working yet) - Homework

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,23 +1,43 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Header from './Header';
 import Footer from './Footer';
 import Note from './Note';
-import notes from '../notes';
-
-function createNotes(noteItem) {
-  return (
-    <Note
-      key={noteItem.key}
-      title={noteItem.title}
-      content={noteItem.content}
-    />
-  );
-}
+import CreateArea from './CreateArea';
 
 export default function App() {
-  return <>
-    <Header />
-    {notes.map(createNotes)}
-    <Footer />
-  </>
+  const [notes, setNotes] = useState([]);
+
+  function addNote(newNote) {
+    setNotes(prevNotes => [...prevNotes, newNote]);
+  }
+
+  function deleteNote(id) {
+    setNotes(prevNotes => {
+      prevNotes.filter((noteItem, index) => {
+        return index !== id;
+      });
+    });
+    console.log(notes);
+  }
+
+  return (
+    <>
+      <Header />
+      <CreateArea
+        onAdd={addNote}
+      />
+      {notes.map((noteItem, index) => {
+        return (
+          <Note
+            key={index}
+            id={index}
+            title={noteItem.title}
+            content={noteItem.content}
+            onDelete={deleteNote}
+          />
+        );
+      })}
+      <Footer />
+    </>
+  );
 }
--- src/components/Note.jsx
+++ src/components/Note.jsx
@@ -1,8 +1,16 @@
 import React from 'react';
 
-export default function Note({ title, content }) {
-  return <div className="note">
-    <h1>{title}</h1>
-    <p>{content}</p>
-  </div>
+export default function Note({ id, title, content, onDelete }) {
+
+  function handleClick() {
+    onDelete(id);
+  }
+
+  return (
+    <div className="note">
+      <h1>{title}</h1>
+      <p>{content}</p>
+      <button onClick={handleClick}>DELETE</button>
+    </div>
+  );
 }
